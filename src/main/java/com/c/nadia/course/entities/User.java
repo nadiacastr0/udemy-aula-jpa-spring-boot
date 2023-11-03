f4646fbc888cd0b85a5f79927c0c861e03ca4f93
Naely Castro <nadianaely32@gmail.com>
2023-11-03 15:18:14 -0300

    H2 database, test profile, JPA

--- src/main/java/com/c/nadia/course/entities/User.java
+++ src/main/java/com/c/nadia/course/entities/User.java
@@ -1,9 +1,15 @@
 package com.c.nadia.course.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb=user")
 public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
@@ -16,6 +22,10 @@ public class User implements Serializable {
         this.email = email;
         this.phone = phone;
         this.password = password;
+    }
+
+    public User() {
+
     }
 
     public Long getId(){ return id;}
